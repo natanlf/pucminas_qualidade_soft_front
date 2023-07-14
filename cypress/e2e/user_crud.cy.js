7bd6ef67692bd4e7ca13ae32cc138bbbbecc0ffb
Natan <natanlaraferreira@gmail.com>
2023-07-14 14:15:23 -0300

    cypress tests

---
+++ cypress/e2e/user_crud.cy.js
@@ -0,0 +1,53 @@
+describe('User CRUD', () => {
+  
+  beforeEach(() => {
+    cy.task('db:erase');
+    cy.visit('http://localhost:3000');
+  });
+
+  it('Deve listar todos os usuários', () => {
+    cy.task('db:create', {
+      name: "Angela",
+      email: "angela_santos@gmail.com",
+      password: "123456"
+    });
+    cy.contains("Angela");
+  });
+
+  it('Deve criar um novo usuário', () => {
+    cy.get('.RaCreateButton-root').click();
+    cy.get('#name').type('James');
+    cy.get('#email').type('james@gmail.com');
+    cy.get('#password').type('senha123');
+    cy.get('.RaToolbar-defaultToolbar > .MuiButtonBase-root').click();
+    cy.contains('Element created');
+    cy.visit('http://localhost:3000');
+    cy.contains('James');
+  });
+
+  it('Deve remover um usuário', () => {
+    cy.task('db:create', {
+      name: "Angela",
+      email: "angela_santos@gmail.com",
+      password: "123456"
+    });
+    cy.get('.MuiTableBody-root > :nth-child(1)').click();
+    cy.get('.ra-delete-button').click();
+    cy.contains('Element deleted');
+  });
+
+  it('Deve atualizar um usuário', () => {
+    cy.task('db:create', {
+      name: "Angela",
+      email: "angela_santos@gmail.com",
+      password: "123456"
+    });
+    cy.get('.MuiTableBody-root > :nth-child(1)').click();
+    cy.get('#name').clear().type('James');
+    cy.get('#email').clear().type('james@gmail.com');
+    cy.get('#password').clear().type('senha123');
+    cy.get('.RaToolbar-defaultToolbar > .MuiButton-contained').click();
+    cy.contains('Element updated');
+    cy.contains('James');
+  });
+});
